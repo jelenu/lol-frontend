bf76e174320e354141ec98eb7b4332db59d65d3f
jelenu <jesusleon2700@gmail.com>
2024-04-04 14:54:37 +0200

    dropdown menu created

--- src/components/summoners/matches/dropdown/Dropdown.jsx
+++ src/components/summoners/matches/dropdown/Dropdown.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { OverView } from './overview/OverView'
+import React, { useState } from 'react';
+import { OverView } from './overview/OverView';
+import { TeamAnalysis } from './TeamAnalysis';
+import { Build } from './Build';
 
-export const Dropdown = ({match}) => {
+export const Dropdown = ({ match }) => {
+  const [selectedComponent, setSelectedComponent] = useState('OverView');
+
+  const handleComponentChange = (component) => {
+    setSelectedComponent(component);
+  };
+
   return (
-    <div>
-      <OverView match={match}/>
+    <div className="">
+      <div className="flex justify-around py-2 bg-gray-200">
+        <button
+          onClick={() => handleComponentChange('OverView')}
+          className={`px-4 py-2 rounded ${
+            selectedComponent === 'OverView' ? 'bg-gray-400 text-white' : 'bg-gray-200 text-gray-700'
+          } mr-2 focus:outline-none`}
+        >
+          OverView
+        </button>
+        <button
+          onClick={() => handleComponentChange('TeamAnalysis')}
+          className={`px-4 py-2 rounded ${
+            selectedComponent === 'TeamAnalysis' ? 'bg-gray-400 text-white' : 'bg-gray-200 text-gray-700'
+          } mr-2 focus:outline-none`}
+        >
+          Team Analysis
+        </button>
+        <button
+          onClick={() => handleComponentChange('Build')}
+          className={`px-4 py-2 rounded ${
+            selectedComponent === 'Build' ? 'bg-gray-400 text-white' : 'bg-gray-200 text-gray-700'
+          } focus:outline-none`}
+        >
+          Build
+        </button>
+      </div>
+      {selectedComponent === 'OverView' && <OverView match={match} />}
+      {selectedComponent === 'TeamAnalysis' && <TeamAnalysis />}
+      {selectedComponent === 'Build' && <Build />}
     </div>
-  )
-}
+  );
+};
